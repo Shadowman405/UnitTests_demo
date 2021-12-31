a534ee402c06c3fd51f4db84a55efaf0764874f7
Maxim Mitin <maximus405@gmail.com>
2021-12-31 19:39:47 +0300

    tests covers all func in VC

--- UnitTests_demoTests/UnitTests_demoTests.swift
+++ UnitTests_demoTests/UnitTests_demoTests.swift
@@ -9,19 +9,20 @@ import XCTest
 @testable import UnitTests_demo
 
 class UnitTests_demoTests: XCTestCase {
+    
+    var sut: ViewController!
 
     override func setUpWithError() throws {
         super.setUp()
-        
+        sut = ViewController()
     }
 
     override func tearDownWithError() throws {
-        
+        sut = nil
         super.tearDown()
     }
     
     func testLowestVolumeShouldBeZero() {
-        let sut = ViewController()
         
         sut.setVolume(value: -100)
         
@@ -30,12 +31,30 @@ class UnitTests_demoTests: XCTestCase {
     }
     
     func testHighestVolumeShouldBeOneHundred() {
-        let sut = ViewController()
-        
         sut.setVolume(value: 200)
         
         let volume = sut.volume
         XCTAssert(volume == 100, "Highest value should be equal 100")
+    }
+    
+    func testCharsInSrtingsAreTheSame() {
+        let string1 = "qwerty"
+        let string2 = "ytrewq"
+        
+        let bool = sut.charactersCompare(stringOne: string1, stringTwo: string2)
+        
+        XCTAssert(bool , "String should be the same")
+        
+    }
+    
+    func testCharsInSrtingsAreTDifferent() {
+        let string1 = "qwerty"
+        let string2 = "ytrewq12"
+        
+        let bool = sut.charactersCompare(stringOne: string1, stringTwo: string2)
+        
+        XCTAssert(!bool , "String should be different")
+        
     }
     
 }
