1eb54e4f3d517df1adf1c308cee76dfd1fbd4ecb
Maxim Mitin <maximus405@gmail.com>
2021-12-31 19:11:44 +0300

    Initial Commit

---
+++ UnitTests_demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnitTests_demo
+//
+//  Created by Maxim Mitin on 31.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
